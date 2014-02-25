5721e12f9199141f57a80c3e23f573bcaea7c23a
CopyLiu <copyliu@gmail.com>
2014-02-25 13:38:23 +0800

    enable x264 10bit

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -347,7 +347,7 @@ generic_configure_make_install() {
 build_x264() {
   do_git_checkout "http://repo.or.cz/r/x264.git" "x264" "origin/stable"
   cd x264
-  local configure_flags="--host=$host_target --enable-static --cross-prefix=$cross_prefix --prefix=$mingw_w64_x86_64_prefix --extra-cflags=-DPTW32_STATIC_LIB --enable-debug" # --enable-win32thread --enable-debug shouldn't hurt us since ffmpeg strips it anyway I think
+  local configure_flags="--host=$host_target --enable-static --cross-prefix=$cross_prefix --prefix=$mingw_w64_x86_64_prefix --extra-cflags=-DPTW32_STATIC_LIB --enable-debug --bit-depth=10" # --enable-win32thread --enable-debug shouldn't hurt us since ffmpeg strips it anyway I think
   if [[ $x264_profile_guided = y ]]; then
     # TODO more march=native here?
     # TODO profile guided here option, with wine?
